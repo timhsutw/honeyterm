35dcdf1b5eb301b7f473306b22d38d620792a5f9
JackKuo <kuohfjack@gmail.com>
2018-01-27 22:34:01 +0800

    change the way to create password

--- honeyterm/honeypot/setup.sh
+++ honeyterm/honeypot/setup.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 syslog_ip=172.17.0.240
 account=guest
-password="\$6\$Y.8OH9VC\$/nIduftdGeWemAm87gWxplL6hga04ibI9DwHxkxWW7dNUApr6BUEuXqHF9DErMIbID2C5A1oY4i2eWZf1U.A6/"
+password="honeypot"
 
-#backup
+# backup
 cp /etc/pam.d/sshd /etc/pam.d/sshd.bak
 cp /etc/rsyslog.d/50-default.conf /etc/rsyslog.d/50-default.conf.bak
 
 # account / password
-useradd -m -p "$password" -s /bin/bash $account
-cat >> /home/$account/.bashrc << _EOF_
+useradd -m  $account -s /bin/bash
+echo "$account:$password"|chpasswd
 
+# run showterm right after login from bashrc
+cat >> /home/$account/.bashrc << _EOF_
 if [ -z \$already_showterm ]; then
 export already_showterm=true
 /usr/local/rvm/wrappers/ruby-2.2.2/showterm
